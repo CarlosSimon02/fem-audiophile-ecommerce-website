66d0707962d4a6ea7663b7812bde225c594ce216
CarlosSimon02 <caloycamacho02@gmail.com>
2024-03-25 20:33:58 +0800

    wrap classnames to cn function

--- components/shared/Container.tsx
+++ components/shared/Container.tsx
@@ -1,3 +1,5 @@
+import { cn } from "@/utils/functions";
+
 type ContainerProps<T extends React.ElementType> = {
   as?: T;
   className?: string;
@@ -14,7 +16,10 @@ export const Container = <T extends React.ElementType = "div">({
 
   return (
     <Component
-      className={`w-full mx-auto max-w-[84rem] px-6 md:px-6 lg:px-8 ${className}`}
+      className={cn(
+        "w-full mx-auto max-w-[71.875rem] px-6 sm:px-10",
+        className
+      )}
     >
       {children}
     </Component>
