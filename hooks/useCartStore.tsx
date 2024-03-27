2fd8ad2c3c27f5c006ddd51662d2b5d4ab2dee7c
CarlosSimon02 <caloycamacho02@gmail.com>
2024-03-27 20:10:22 +0800

    added ability to store in localstorage

--- hooks/useCartStore.tsx
+++ hooks/useCartStore.tsx
@@ -1,10 +1,9 @@
 import { CartItem } from "@/utils/types";
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 type State = {
   items: CartItem[];
-  count: () => number;
-  totalAmount: () => number;
 };
 
 type Actions = {
@@ -14,45 +13,46 @@ type Actions = {
   updateQuantityOfItem: (id: number, quantity: number) => void;
 };
 
-const useCartStore = create<State & Actions>()((set, get) => {
-  return {
-    items: [],
-    count: () =>
-      get().items.reduce((sum, { quantity }) => {
-        return sum + quantity;
-      }, 0),
-    totalAmount: () =>
-      get().items.reduce((sum, { price, quantity }) => {
-        return sum + price * quantity;
-      }, 0),
-    addItem: (newItem: CartItem) =>
-      set((state) => {
-        //check if item already exist in array
-        const existingItem = state.items.find((item) => item.id === newItem.id);
-
-        if (existingItem) {
-          //Just update the quantity
-          const updatedItems = state.items.map((item) =>
-            item.id === newItem.id
-              ? { ...item, quantity: item.quantity + newItem.quantity }
-              : item
+const useCartStore = create<State & Actions>()(
+  persist(
+    (set, get) => ({
+      items: [],
+      addItem: (newItem: CartItem) =>
+        set((state) => {
+          //check if item already exist in array
+          const existingItem = state.items.find(
+            (item) => item.id === newItem.id
           );
 
-          return { items: [...updatedItems] };
-        } else {
-          return { items: [...state.items, newItem] };
-        }
-      }),
-    removeItem: (id) =>
-      set((state) => ({ items: state.items.filter((item) => item.id !== id) })),
-    removeAllItems: () => set(() => ({ items: [] })),
-    updateQuantityOfItem: (id, quantity) =>
-      set((state) => ({
-        items: state.items.map((item) => {
-          return item.id === id ? { ...item, quantity: quantity } : item;
+          if (existingItem) {
+            //Just update the quantity
+            const updatedItems = state.items.map((item) =>
+              item.id === newItem.id
+                ? { ...item, quantity: item.quantity + newItem.quantity }
+                : item
+            );
+
+            return { items: [...updatedItems] };
+          } else {
+            return { items: [...state.items, newItem] };
+          }
         }),
-      })),
-  };
-});
+      removeItem: (id) =>
+        set((state) => ({
+          items: state.items.filter((item) => item.id !== id),
+        })),
+      removeAllItems: () => set(() => ({ items: [] })),
+      updateQuantityOfItem: (id, quantity) =>
+        set((state) => ({
+          items: state.items.map((item) => {
+            return item.id === id ? { ...item, quantity: quantity } : item;
+          }),
+        })),
+    }),
+    {
+      name: "cart-storage",
+    }
+  )
+);
 
 export default useCartStore;
