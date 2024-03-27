3e8048fd4dcead6aa7df8a1c2f35bfa499559a3c
CarlosSimon02 <caloycamacho02@gmail.com>
2024-03-27 20:09:08 +0800

    seperate modal functionality to hook

--- components/shared/MobileNavControl.tsx
+++ components/shared/MobileNavControl.tsx
@@ -1,59 +1,52 @@
 "use client";
-import { useOuterClick } from "@/hooks/useOuterClick";
+import useModal from "@/hooks/useModal";
 import { cn } from "@/utils/functions";
-import { useRef, useState } from "react";
 import { CSSTransition } from "react-transition-group";
 import Overlay from "../ui/Overlay";
 import Portal from "../ui/Portal";
 import { HamburgerIcon } from "../ui/SVGs";
 import CategoryNavLinks from "./CategoryNavLinks";
-import { Container } from "./Container";
+import Container from "./Container";
 
 const MobileNavControl = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const menuRef = useRef<HTMLDivElement>(null);
-
-  const handleOpenCloseMenu = () => {
-    setIsMenuOpen((prev) => (prev ? false : true));
-  };
-
-  useOuterClick(menuRef, handleOpenCloseMenu);
+  const { isModalOpen, modalRef, handleOpenModal, handleCloseModal } =
+    useModal();
 
   return (
     <>
       <button
         className={cn(
-          isMenuOpen ? "pointer-events-none" : undefined,
+          isModalOpen ? "pointer-events-none" : undefined,
           "shrink-0 sm:max-lg:mr-10 lg:hidden"
         )}
-        aria-label={isMenuOpen ? "Close Menu" : "Open Menu"}
+        aria-label={isModalOpen ? "Close Menu" : "Open Menu"}
         aria-haspopup="true"
-        onClick={handleOpenCloseMenu}
+        onClick={handleOpenModal}
       >
         <HamburgerIcon className="w-4" />
       </button>
       <Portal parentElement=".main-header">
         <CSSTransition
-          in={isMenuOpen}
-          nodeRef={menuRef}
+          in={isModalOpen}
+          nodeRef={modalRef}
           timeout={300}
-          classNames="menu"
+          classNames="dialog"
           unmountOnExit
         >
           <div
             className={"bg-light-100 pt-24 pb-16 text-dark-500 w-full absolute"}
-            ref={menuRef}
+            ref={modalRef}
           >
             <Container>
               <CategoryNavLinks
                 className="!m-0"
-                onNavigate={handleOpenCloseMenu}
+                onNavigate={handleCloseModal}
               />
             </Container>
           </div>
         </CSSTransition>
       </Portal>
-      <Overlay isOpen={isMenuOpen} />
+      <Overlay isOpen={isModalOpen} />
     </>
   );
 };
