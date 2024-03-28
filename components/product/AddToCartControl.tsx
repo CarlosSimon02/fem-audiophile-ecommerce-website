cf0b96db9e7fca2e2be3795abddd267ced9b19f4
CarlosSimon02 <caloycamacho02@gmail.com>
2024-03-28 20:05:31 +0800

    add functionality

--- components/product/AddToCartControl.tsx
+++ components/product/AddToCartControl.tsx
@@ -1,12 +1,14 @@
 "use client";
 import useCartStore from "@/hooks/useCartStore";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import FloatingBar from "../ui/FloatingBar";
 import InputNumber from "../ui/InputNumber";
+import { CloseIcon } from "../ui/SVGs";
 
 type AddToCartControlProps = {
   itemToAdd: {
     id: number;
-    image: string;
+    slug: string;
     shortName: string;
     price: number;
   };
@@ -14,10 +16,41 @@ type AddToCartControlProps = {
 
 const AddToCartControl = ({ itemToAdd }: AddToCartControlProps) => {
   const [quantity, setQuantity] = useState(1);
+  const [isExceeded, setIsExceeded] = useState(false);
+  const [isFloatingBarOpen, setIsFloatingBarOpen] = useState(false);
+  const cartItems = useCartStore((state) => state.items);
   const addItemToCart = useCartStore((state) => state.addItem);
 
-  const handleClick = () => {
-    addItemToCart({ ...itemToAdd, quantity });
+  useEffect(() => {
+    if (isFloatingBarOpen) {
+      setTimeout(() => {
+        setIsFloatingBarOpen(false);
+      }, 3000);
+    }
+  }, [isFloatingBarOpen]);
+
+  const handleAddToCartClick = () => {
+    const itemQuantity = cartItems.find(
+      ({ id }) => itemToAdd.id === id
+    )?.quantity;
+
+    if (!itemQuantity) {
+      addItemToCart({ ...itemToAdd, quantity });
+      setIsExceeded(false);
+    } else {
+      if (itemQuantity + quantity > 999) {
+        setIsExceeded(true);
+      } else {
+        setIsExceeded(false);
+        addItemToCart({ ...itemToAdd, quantity });
+      }
+    }
+
+    setIsFloatingBarOpen(true);
+  };
+
+  const handleCloseFloatingBarClick = () => {
+    setIsFloatingBarOpen(false);
   };
 
   return (
@@ -26,9 +59,35 @@ const AddToCartControl = ({ itemToAdd }: AddToCartControlProps) => {
         initValue={quantity}
         onValueChange={(value: number) => setQuantity(value)}
       />
-      <button className="accent-button" onClick={handleClick}>
+      <button className="accent-button" onClick={handleAddToCartClick}>
         Add to Cart
       </button>
+      <FloatingBar
+        isOpen={isFloatingBarOpen}
+        className="flex gap-3 items-center"
+      >
+        {isExceeded ? (
+          <>
+            <span className="w-7 h-7 bg-red-500 text-light-100 flex items-center justify-center rounded-full">
+              ✘
+            </span>
+            <p className="flex-1">Maximum number of items is reached</p>
+            <button onClick={handleCloseFloatingBarClick}>
+              <CloseIcon className="w-4 h-4" />
+            </button>
+          </>
+        ) : (
+          <>
+            <span className="w-7 h-7 bg-green-500 text-light-100 flex items-center justify-center rounded-full">
+              ✓
+            </span>
+            <p className="flex-1">Item is succesfully added to cart</p>
+            <button onClick={handleCloseFloatingBarClick}>
+              <CloseIcon className="w-4 h-4" />
+            </button>
+          </>
+        )}
+      </FloatingBar>
     </div>
   );
 };
